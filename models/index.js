3c15fd51abf9e2401a3ef6811417987d514f2777
juanx198522 <juanx1985@icloud.com>
2023-07-12 19:58:12 -0400

    Test

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./user.js');
+const Cardio = require('./cardio.js');
+const Community = require('./community.js');
+const Goals = require('./goals.js');
+const Progress = require('./progress.js');
+const Strength = require('./strength.js');
+const Yoga = require('./yoga.js');
+
+module.exports = { User, Cardio, Community, Goals, Progress, Strength, Yoga }; 
