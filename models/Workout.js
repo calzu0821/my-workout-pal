0b4ce22f9a1dc0b3da6d2ee3ca8910b0ee04ba01
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-07-12 21:38:00 -0400

    Revised Seeds/Models

--- models/Workout.js
+++ models/Workout.js
@@ -20,7 +20,7 @@ Workout.init(
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     duration: {
       type: DataTypes.INTEGER,
