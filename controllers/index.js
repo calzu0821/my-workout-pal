e4e2ab3bcf2e5fadfc9c755144b4b318bc99ee0c
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-07-13 19:18:49 -0400

    push for TA

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes')
+const apiRoutes = require('./api')
 
+// localhost:3001/
 router.get('/', homeRoutes )
+
+// localhost:3001/api
+router.get('/api', apiRoutes)
 
 
 module.exports = router;
