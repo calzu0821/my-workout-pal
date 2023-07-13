40e8b40cf8582ddede51955215e99002326825b8
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-07-12 21:07:23 -0400

    Revised models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User.js');
-const Workout = require('./Workout.js');
 const Community = require('./Community.js');
+const Workout = require('./Workout.js');
 
 
 module.exports = { User, Workout, Community };
