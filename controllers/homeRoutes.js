c7c1727968b8fa39f7ac073db34db75d4c5f7a7e
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-07-15 17:17:24 -0400

    added import modules and began working on the routers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,25 @@
-const { Community } = require('../models');
-
+// Import modules
 const router = require('express').Router();
+const { Workout, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// const { Community } = require('../models');
+
+// const router = require('express').Router();
 
 // localhost:3001/
 router.get('/', (req, res) => {
     res.render('homepage');
 })
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/exercise');
+      return;
+    }
+    res.render('login');
+  });
 
 router.get('/community', async (req, res) => {
     try {
