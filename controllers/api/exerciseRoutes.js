a9aded98c155579bbb4de64167a63db5eca80695
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-07-17 20:24:26 -0400

    Revised /exerciseRoutes

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -3,30 +3,27 @@ const router = require('express').Router();
 const { Workout, Community, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
-
-  router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const workoutData= await Workout.findAll({
-            where: {
-              id: req.params.id,
-              user_id: req.session.id,
-            },
-        });
-
-        console.log(workoutData)
+      const workoutData = await Workout.findByPk(req.params.user_id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
   
-        const workoutMap = workoutData.map((workout) => workout.get({ plain: true }));
-        console.log(workoutData)
-        // Pass serialized data and session flag into template
-        res.render('exercise', { 
-          workoutMap, logged_in: req.session.logged_in
-        });
-      } catch (err) {
-          console.log(err)
-        res.status(500).json(err);
-           
-      }
+      const workoutMap = workoutData.get({ plain: true });
+  
+      res.render('workout', {
+        ...Workout,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
+
 
   module.exports = router;
