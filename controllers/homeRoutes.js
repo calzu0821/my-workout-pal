e14241459196fdbaca59163a12a193a01b9279c9
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-07-19 19:25:18 -0400

    Revised /exercise route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,13 +4,14 @@ const { Workout, Community, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+res.render('homepage', {logged_in: req.session.logged_in});
+  
 });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/exercise');
+    res.redirect('/userexercise');
   } else {
     res.render('login');
   }
